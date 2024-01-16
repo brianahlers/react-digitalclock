62205c3b21f0d7976da9012f222060cce3ba6e3d
Brian A <brian.ahlers@gmail.com>
2024-01-16 11:11:20 -0800

    digital clock component added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import DigitalClock from './DigitalClock'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <DigitalClock />
+  )
+}
+
+export default App
